304244ba3fc53be55e748f69b5712cb2baaad040
Carvalh0R0b3rt <carvalhorobert561@gmail.com>
2025-10-22 20:18:32 -0300

    Código Base

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
 #New code
+
+peso = float(input("Informe o seu peso atual:"))
+altura = float(input("Agora informe sua altura, em metros:"))
+
+def calcular_imc (peso, altura):
+    imc = peso/(altura*altura)
+    return imc
+
+resultado_imc = calcular_imc(peso, altura)
+print("O resultado do IMC para estes valores é: ", resultado_imc)
